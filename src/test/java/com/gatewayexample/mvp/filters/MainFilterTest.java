8ba122fa218943962a57484db1f56b12648e342c
Foteno <patrikass32@gmail.com>
2025-01-13 00:29:32 +0200

    example

--- src/test/java/com/gatewayexample/mvp/filters/MainFilterTest.java
+++ src/test/java/com/gatewayexample/mvp/filters/MainFilterTest.java
@@ -1,17 +1,15 @@
 package com.gatewayexample.mvp.filters;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.mock.web.server.MockServerWebExchange;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class TestGatewayFilterTest {
@@ -29,7 +27,7 @@ class TestGatewayFilterTest {
 		Mono<Void> result = testGatewayFilter.filter(exchange, e -> Mono.empty());
 		StepVerifier.create(result)
 				.verifyComplete();
-
+		//assertion passes with spring boot 3.3.x and spring cloud gateway dependencies 4.1.x but not 3.4.x and 4.2.x respectively
 		assertEquals("test", exchange.getRequest().getHeaders().getFirst("test-header"));
 	}
 }
